ff08027f702e7d495492738e6de976a8ed71e938
biggest0 <thebiggestkai@gmail.com>
2025-11-16 23:31:15 -0800

    add move code logic into article service

--- lib/core/api/article_api.dart
+++ lib/core/api/article_api.dart
@@ -1,12 +1,9 @@
 import 'dart:convert';
-import 'package:catire_mobile/core/models/article_model.dart';
 import 'package:http/http.dart' as http;
 
 import "package:catire_mobile/core/constants/api_routes.dart";
 
-import '../utils/data_formatter.dart';
-
-Future<ArticleDetail> fetchArticleDetail(String articleId) async {
+Future<Map<String, dynamic>> fetchArticleDetail(String articleId) async {
   final response = await http.post(
     Uri.parse(ApiRoutes.API_ARTICLE_DETAIL),
 
@@ -18,11 +15,7 @@ Future<ArticleDetail> fetchArticleDetail(String articleId) async {
 
   if (response.statusCode == 201 || response.statusCode == 200) {
     var jsonObject = jsonDecode(response.body) as Map<String, dynamic>;
-
-    final articleResponse = ArticleDetailResponse.fromJson(jsonObject);
-    final articleDetail = articleDetailTransform(articleResponse);
-
-    return articleDetail;
+    return jsonObject;
   } else {
     final errorMessage =
         'Failed to fetch article detail. Error: $response.statusCode - $response.body';
@@ -30,41 +23,18 @@ Future<ArticleDetail> fetchArticleDetail(String articleId) async {
   }
 }
 
-Future<List<ArticleInfoResponse>> fetchArticles({
-  int page = 1,
-  int limit = 10,
-  String? category,
-  String? search,
-  String? dateRange,
-  String? sortBy,
-}) async {
-  final queryParams = <String, String>{
-    'page': page.toString(),
-    'limit': limit.toString(),
-    if (category != null) 'category': category,
-    if (search != null) 'search': search,
-    if (dateRange != null) 'dateRange': dateRange,
-    if (sortBy != null) 'sortBy': sortBy,
-  };
-
+Future<List<dynamic>> fetchArticles(Map<String, String> queryParams) async {
   final response = await http.get(
-    Uri.parse(
-      ApiRoutes.API_ARTICLE_DETAIL,
-    ).replace(queryParameters: queryParams),
+    Uri.parse(ApiRoutes.API_ARTICLE_INFO).replace(queryParameters: queryParams),
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     },
   );
 
   if (response.statusCode == 200) {
-    final List<dynamic> data = jsonDecode(response.body);
+    final List<dynamic> jsonList = jsonDecode(response.body);
 
-    return data
-        .map(
-          (jsonArticle) =>
-              ArticleInfoResponse.fromJson(jsonArticle as Map<String, dynamic>),
-        )
-        .toList();
+    return jsonList;
   } else {
     final errorMessage =
         'Failed to fetch articles. Error: $response.statusCode - $response.body';
---
+++ lib/core/services/article_service.dart
@@ -0,0 +1,48 @@
+import 'dart:developer';
+import 'package:catire_mobile/core/models/article_model.dart';
+
+import '../api/article_api.dart';
+import '../utils/data_formatter.dart';
+
+Future<ArticleDetail?> getArticleDetail(String articleId) async {
+  try {
+    final data = await fetchArticleDetail(articleId);
+    final articleResponse = ArticleDetailResponse.fromJson(data);
+    final articleDetail = articleDetailTransform(articleResponse);
+
+    return articleDetail;
+  } catch (error) {
+    log("Failed to get article detail:$error");
+    return null;
+  }
+}
+
+Future<List<ArticleInfoResponse>?> getArticles({
+  int page = 1,
+  int limit = 10,
+  String? category,
+  String? search,
+  String? dateRange,
+  String? sortBy,
+}) async {
+  try {
+    final queryParams = <String, String>{
+      'page': page.toString(),
+      'limit': limit.toString(),
+      if (category != null) 'category': category,
+      if (search != null) 'search': search,
+      if (dateRange != null) 'dateRange': dateRange,
+      if (sortBy != null) 'sortBy': sortBy,
+    };
+    final data = await fetchArticles(queryParams);
+    return data
+        .map(
+          (jsonArticle) =>
+              ArticleInfoResponse.fromJson(jsonArticle as Map<String, dynamic>),
+        )
+        .toList();
+  } catch (error) {
+    log("Failed to get article info:$error");
+    return null;
+  }
+}
