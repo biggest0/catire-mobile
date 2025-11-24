24f12492c0f573179384cd991fbfd9aaa8325484
biggest0 <thebiggestkai@gmail.com>
2025-11-23 21:13:43 -0800

    implement profile picture widget

---
+++ lib/features/account/profile_picture.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ProfilePicture extends StatelessWidget {
+  final String imagePath;
+
+  const ProfilePicture({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      radius: 60,
+      backgroundColor: Colors.grey[300],
+      backgroundImage: AssetImage(imagePath),
+    );
+  }
+}
