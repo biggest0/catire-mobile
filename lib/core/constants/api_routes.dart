0c334d2237ec4d44d4c0bda5b47628800fd8d175
biggest0 <thebiggestkai@gmail.com>
2025-11-27 23:19:29 -0800

    add splash screen and logo

---
+++ lib/core/constants/api_routes.dart
@@ -0,0 +1,7 @@
+class ApiRoutes {
+  static const String API_URL = "https://catire-1acdb920c122.herokuapp.com";
+  static const String API_ARTICLE_DETAIL = "$API_URL/article-detail";
+  static const String API_ARTICLE_INFO = "$API_URL/article-info";
+  static const String API_TOP_TEN = "$API_URL/article-top-ten";
+  static const String API_INCREMENT_VIEWED = "$API_URL/increment-article-view";
+}
